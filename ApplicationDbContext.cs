f54f0e1c83ebbfee3428da902c56bdad2dc29dc8
Trevor <reevest2@gmail.com>
2023-01-06 00:31:17 -0500

    added local db connection

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UntitiledProject
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WeatherForecast> WeatherForecast { get; set; }
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UntitiledProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            IEnumerable<WeatherForecast> weatherForecasts = _context.WeatherForecast.ToList();
+            return weatherForecasts;
+        }
+    }
+}
